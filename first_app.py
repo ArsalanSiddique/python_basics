ecaec9552bba67d9ca5effd0e204723febf3363a
ArsalanSiddique <arsalan.ahmed1264@gmail.com>
2021-10-31 03:52:16 +0500

    started

---
+++ first_app.py
@@ -0,0 +1,26 @@
+# basics of python
+# Class: 01
+# Date: 31/Oct
+
+
+print("Hello World!")
+name = "Arsalan"
+print(name)
+number = 2
+print(number)
+print(number+2)
+number_two = 22
+print(number_two-number)
+total_cost = (2 * 4) * 4 + 2
+print(total_cost)
+
+
+if total_cost == 30+4+1:
+    print("wow")
+    print("working")
+elif total_cost == 34:
+    print("total cost: ",total_cost, " & now",number_two)
+else:
+    print("I'm in else")
+
+print("outside if")
